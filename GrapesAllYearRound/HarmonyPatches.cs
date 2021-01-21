80c7397a4da6ad40213a7a842ffe0e905f1a8c49
Gleb Zhynko <gleb.zhinko@gmail.com>
2021-01-21 16:08:29 +0300

    add GrapesAllYearRound mod files

---
+++ GrapesAllYearRound/HarmonyPatches.cs
@@ -0,0 +1,34 @@
+using System;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace GrapesAllYearRound
+{
+    public class HarmonyPatches
+    {
+        /// <summary> Patch for the Crop.newDay method. </summary>
+        public static void CropNewDay(Crop __instance, int state, int fertilizer, int xTile, int yTile,
+            GameLocation environment)
+        {
+            if (__instance.indexOfHarvest != 398) return; // return if this crop is not grape.
+            
+            // Return if this crop is still growing,
+            // its hoe dirt is not watered or this crop is fiber.
+            if ((!__instance.fullyGrown && (__instance.dayOfCurrentPhase != 0 && __instance.currentPhase != 5)) ||
+                state != 1 || __instance.indexOfHarvest == 771) return;
+            
+            if (Game1.currentSeason == "fall") return;
+            
+            // If the crop is about to finish its growth in any season other than fall,
+            // prevent it from producing.
+            if (__instance.currentPhase == 5 && __instance.dayOfCurrentPhase == 0)
+            {
+                __instance.dayOfCurrentPhase.Value = 3;
+                __instance.fullyGrown.Value = true;
+            }
+                
+            // Keep the day of the current phase the same throughout the year.
+            __instance.dayOfCurrentPhase.Value++;
+        }
+    }
+}
---
+++ GrapesAllYearRound/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GrapesAllYearRound")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GrapesAllYearRound")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4367BAEA-29EE-47A7-8017-FB8C9177B698")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
